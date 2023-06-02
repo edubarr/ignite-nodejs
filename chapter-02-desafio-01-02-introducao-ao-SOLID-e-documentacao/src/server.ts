e46b2e57723890962fe726aff7a7023f6c35ec1a
edubarr <eduaraujobarros@gmail.com>
2023-06-02 14:57:40 -0300

    Chapter 02 - Desafio 02

--- chapter-02-desafio-01-02-introducao-ao-SOLID-e-documentacao/src/server.ts
+++ chapter-02-desafio-01-02-introducao-ao-SOLID-e-documentacao/src/server.ts
@@ -1,5 +1,10 @@
+import swaggerUi from "swagger-ui-express";
+
 import { app } from ".";
 import { usersRoutes } from "./routes/users.routes";
+import swaggerFile from "./swagger.json";
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(usersRoutes);
 
